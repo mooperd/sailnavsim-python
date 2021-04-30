38a780f28ce8ba4446c048bee9e4464f85b95bd6
Andrew Holway <andrew.holway@gmail.com>
2021-04-30 18:15:59 +0000

    exporting secrets properly.. hopefully

---
+++ sailnavsim_web/root.py
@@ -0,0 +1,20 @@
+"""Logged-in page routes."""
+from flask import Blueprint, flash, redirect, render_template, url_for, request
+
+# Blueprint Configuration
+root_bp = Blueprint(
+    'root_bp', __name__
+)
+
+
+@root_bp.route("/")
+def root():
+    """Health check"""
+    return redirect(url_for('pages_bp.races'))
+
+
+@root_bp.route("/healthz")
+def healthz():
+    """Health check"""
+    return "OK"
+
